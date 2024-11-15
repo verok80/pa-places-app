af1bd71840da8819d5b3fbcfe0df0ba2a7482ad3
Vera Kolotyuk <verok@Veras-MacBook-Pro.local>
2024-11-15 10:32:31 -0500

    added src

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+import { data } from './data';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [places, setPlaces] = useState(data);
+  console.log(data)
+  const [showText, setShowText] = useState(false);
+  const checkPlaces = (id) => {
+    let newPlaces = places.filter(place => place.id !==id);
+    setPlaces(newPlaces)
+  }
+ const clickText = (element) =>{
+  element.showMore =!element.showMore
+  setShowText(!showText)
+ }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="container">
+        <h1>{ places.length } places to visit in Pennsylvania </h1>
+      </div>
+      {places.map((element =>{
+        const {id, name, image, imageSource, description,showMore} = element;
+        return(
+          <div key= {id}>
+            <div className="container">
+              <h2>{id} - {name}</h2>
+            </div>
+            
+            <div className="container">
+              <img src={ image }  alt="view" width="600px"/>
+            </div>
+            <div className="container">
+            <p className ="source">Image source: { imageSource}</p>
+            </div>
+            <div className="container">
+              <p className="description">{showMore ? description : description.substring(0,300) + "...." }
+              <button onClick = {() => clickText(element)}>{showMore? "Show less" : "Show more"}</button>
+              </p>
+            </div>
+             <div className="container">
+              <button className="btn" onClick = { () => checkPlaces(id)} >Check</button>
+            </div>
+          </div>
+        )
+      }))}
+      
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,98 @@
+import imageOne from './garden.jpeg';
+import imageTwo from './japanese.jpeg';
+import imageThree from './PleaseTouch.jpeg';
+import imageFour from './Fairmount.jpeg';
+import imageFive from './Zoo.jpeg';
+import imageSix from './CherrySprings.jpeg';
+import imageSeven from './HawkMount.jpeg';
+import imageEight from './PhippsBotanical.jpeg';
+import imageNine from './Art.jpeg';
+import imageTen from './PromisedLand.jpeg';
+
+
+
+
+export const data = [
+    {
+        id: 1,
+        name: "Longwood Gardens",
+        image: imageOne,
+        imageSource: "bpprodstorage.blob.core.windows.net",
+        description: "Longwood Gardens is one of the leading botanical gardens in the country. This natural oasis is replete with 40 seasonal gardens that are home to over 10,000 varieties of flora and plant life. They feature indoor and outer spaces, as well as unrivaled heated conservatories. In summer, the rocky Hillside Garden is doted with waterfalls and shrubs, while in winter visitors can admire the blooming Acacia Passage and Acacia Leprosa. Other spaces are named Italian Water Garden, Mediterranean Garden, Peirce's Wood, Fern Passage and Waterlily Display. They also offer workshops and programs in horticulture, botanical science and landscape design, as well as organize concerts a  ",
+        showMore: false   
+       
+    },
+    {
+        id: 2,
+        name: "Shofuso Japanese House & Garden",
+        image: imageTwo,
+        imageSource: "media.timeout.com",
+        description: " Designed by architect Junzo Yoshimura, Shofuso was built in Japan in 1953 using traditional techniques and materials. It was shipped to New York and exhibited in the courtyard of the Museum of Modern Art in New York before moving to West Fairmount Park in 1958. In 2007, international artist Hiroshi Senju, inspired by the garden’s waterfall, donated 20 contemporary murals to Shofuso, which are permanently displayed inside the house. This historic site and museum includes a hill and pond garden with a tiered waterfall, island, and koi fish, a tea garden featuring a traditional tea house, and a courtyard garden leading to a bathhouse.  ",
+        showMore: false
+    },
+    {
+        id: 3,
+        name: "Please Touch Museum",
+        image: imageThree,
+        imageSource: "goop-img.com",
+        description: " Please Touch Museum is a must-visit when in town. Located in Memorial Hall, it features whimsical installations, interactive exhibits and optical illusions. Create music with your feet by stepping on the Walking Piano, meet characters from Alice in Wonderland, cross rivers in the rainforest and manoeuvre a flying machine. Visitors can attend activities and programs on various topics, from history to science, in an educational yet fun way. Make a pit stop at their Please Taste Café, or attend a folk performance or puppet show at their theatre. A popular attraction among children is their wooden carousel and yearly themed Storybook Ball where their favourite fairy tales come alive. ",
+        showMore: false
+    },
+    {
+        id: 4,
+        name: "Fairmount Park",
+        image: imageFour,
+        imageSource: "myphillypark.org",
+        description: " Spreading across 9200 acres (3723.11 hectares) with 63 parks, Fairmount Park is among the biggest city park systems in the nation. It features picturesque trails, rolling hills, streams, historical structures, woodlands, public arts and more. The Centennial Arboretum, Horticulture Center, Japanese House and Garden and Philadelphia Museum of Art are some of the interesting sites located in this massive expanse. It also has recreational centers and sporting fields.",
+        showMore: false
+    },
+    {
+        id: 5,
+        name: "Philadelphia Zoo",
+        image: imageFive,
+        imageSource: "media.timeout.com",
+        description: " As America’s first zoo, Philadelphia Zoo has over 160 years of experience in conserving wildlife, caring for animals, and educating our communities. Whether you’re teaching, learning from home, or coming in for a visit, Philly Zoo to YOU allows you to experience wildlife in a totally new way!",
+        showMore: false
+    },
+    {
+        id: 6,
+        name: "Cherry Springs State Park",
+        image: imageSix,
+        imageSource: "whereandwhen.com",
+        description: " Cherry Springs State Park is nearly as remote and wild today as it was two centuries ago.Named for the large stands of black cherry trees originally found in the area, the 82-acre state park is surrounded by the 262,000-acre Susquehannock State Forest. The Susquehannock Trail passes nearby and offers 85 miles of backpacking and hiking.Night sky enthusiasts flock to the park for its dark skies which are famous for great views of the Milky Way, planets, and hard-to-see astronomical objects and phenomena.",
+        showMore: false
+    },
+    {
+        id: 7,
+        name: "Hawk Mountain Sanctuary",
+        image: imageSeven,
+        imageSource: "upload.wikimedia.org",
+        description: "A visit to the Hawk Mountain is a visit to a multi purpose destination, along with scientific research center, international conservation training site, eco tourism destination, learning facilities for people of all ages, wildlife sanctuary, conservation organisation, etc. this place is a great visit site where species are well protected and are visible here. The Hawk Mountain is one of the best places in the northeastern North America to view the beauty of the hawk migration in autumn.",
+        showMore: false
+    
+    },
+    {
+        id: 8,
+        name: "Phipps Conservatory and Botanical Gardens",
+        image: imageEight,
+        imageSource: "i0.wp.com",
+        description: " Phipps Conservatory and Botanical Gardens is a botanical garden set in Schenley Park, Pittsburgh, Pennsylvania, United States. It is a City of Pittsburgh historic landmark and is listed on the National Register of Historic Places.The gardens were founded in 1893 by steel and real-estate magnate Henry Phipps Jr. as a gift to the City of Pittsburgh. Its purpose is to educate and entertain the people of Pittsburgh with formal gardens and various species of exotic plants. Currently, the facilities house elaborate gardens within the fourteen room conservatory itself and on the adjoining grounds. In addition to its primary flora exhibits, the sophisticated glass and metalwork of the Lord & Burnham conservatory offers an interesting example of Victorian greenhouse architecture.",
+        showMore: false
+    },
+    {
+        id: 9,
+        name: "Philadelphia Museum of Art",
+        image: imageNine,
+        imageSource: "assets.philamuseum.org",
+        description: " The Philadelphia Museum of Art houses a stunning collection of over 240,000 objects, showcasing treasures that span continents and cultures. This remarkable collection is part of a larger compilation of more than 400,000 works of art, including prints, drawings, photographs, and more. The museum itself is an architectural marvel, with its imposing stone edifice supported by majestic Doric columns, standing proudly above the Schuylkill River. Visitors are invited to explore a world of art and history within its walls and don’t forget to channel your inner Rocky by scaling the iconic steps made famous in the classic film series",
+        showMore: false
+    },
+    {
+        id: 10,
+        name: "Promised Land State Park",
+        image: imageTen,
+        imageSource: " i.pinimg.com ",
+        description: " About 3,000 acres in size, Promised Land State Park is on the Pocono Plateau, 1,800 feet above sea level, and is surrounded by 12,464 acres of the Delaware State Forest, including natural areas. Visitors can enjoy:fishing and boating in two lakes, rustic cabin, camping, miles of hiking trails, exploring the forests.The forests of the park consist primarily of beech, oak, maple, and hemlock trees. Two lakes and several small streams add to the park’s outstanding scenic beauty.",
+        showMore: false
+    }
+]
